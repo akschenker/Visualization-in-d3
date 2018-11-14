c352a41a977694076cce8ecdddc8af2550104482
Arley Schenker <akschenker@gmail.com>
2018-11-14 00:55:41 -0800

    added support for tool-tips

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 var svgWidth = 960;
 var svgHeight = 500;
 
-var margin = { top: 20, right: 40, bottom: 60, left: 50 };
+var margin = { top: 20, right: 40, bottom: 60, left: 100 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
@@ -26,7 +26,6 @@ d3.csv("assets/data/data.csv").then(function(data) {
         d.income = +d.income;
         d.obesity = +d.obesity;
     });
-    console.log("print1");
 
     var xLinearScale = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.obesity)])
@@ -35,8 +34,6 @@ d3.csv("assets/data/data.csv").then(function(data) {
     var yLinearScale = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.income)])
     .range([height, 0]);
-
-    console.log("print2");
 
      // Create axis functions
     var bottomAxis = d3.axisBottom(xLinearScale);
@@ -54,22 +51,62 @@ d3.csv("assets/data/data.csv").then(function(data) {
     var circlesGroup = chartGroup.selectAll("circle")
       .data(data)
       .enter()
-      .append("circle")
+
+      var circles = circlesGroup.append("circle")
       .attr("cx", d => xLinearScale(d.obesity))
       .attr("cy", d => yLinearScale(d.income))
       .attr("r", "10")
-      .attr("fill", "blue")
-      .attr("stroke-width", "1")
-      .attr("stroke", "black");
+      .classed("stateCircle", true)
 
-    var abbrGroup = chartGroup.selectAll("text")
-      .data(data)
-      .enter()
-      .append("text")
-      .attr("x", d => xLinearScale(d.obesity) - 3)
-      .attr("y", d => yLinearScale(d.income) + 2)
+      var text = circlesGroup.append("text")
+      .attr("x", d => xLinearScale(d.obesity) - 7)
+      .attr("y", d => yLinearScale(d.income) + 3)
       .attr("fill", "white")
       .text(d => d.abbr)
       .style("text-align", "left")
       .style("font-size", "10px")
+      .style("font-weight", "bold")
+
+      // Step 1: Initialize Tooltip
+    var toolTip = d3.tip()
+      .attr("class", "d3-tip")
+      .html(function(d) {
+        return (`State: ${d.abbr}<br>Obesity: ${(d.obesity)}<br>Income: $${d.income}
+      `);
+    });
+
+    // Step 2: Create the tooltip in circles and text.
+    chartGroup.call(toolTip);
+
+    // Step 3: Create "mouseover" event listener to display tooltip
+    circles.on("mouseover", function(d) {
+      toolTip.show(d, this);
+    })
+    // Step 4: Create "mouseout" event listener to hide tooltip
+    .on("mouseout", function(d) {
+      toolTip.hide(d, this);
+    });
+
+    // Create mouseover/mouseout for text also
+    text.on("mouseover", function(d) {
+      toolTip.show(d, this);
+    })
+    // Step 4: Create "mouseout" event listener to hide tooltip
+    .on("mouseout", function(d) {
+      toolTip.hide(d, this);
+    });
+
+    // Create axes labels
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - (margin.left / 2) - 20)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Yearly Income ($)");
+
+    chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("% Obesity");
 });
